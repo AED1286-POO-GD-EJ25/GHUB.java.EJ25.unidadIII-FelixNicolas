1363ea08c18b9293b9f820e50b7889c905bf8da2
NicolasFelixAG <L24171081@culiacan.tecnm.mx>
2025-03-27 15:57:16 +0000

    uso declases

---
+++ miPrincipal/Libro.java
@@ -0,0 +1,44 @@
+package miPrincipal;
+import java.util.Date;
+public class Libro extends Publicacion{
+     //atributos
+     private String isbn;
+     private String autor;
+     //constructores
+     public Libro(){
+         super();
+ 
+     }
+     public Libro(String isbn,String autor){
+         super("sin definir",null);
+         this.isbn = isbn;
+         this.autor = autor;
+     }
+     public Libro(String editorial, Date fecha, String isbn, String autor){
+         super(editorial, fecha);
+         this.isbn = isbn;
+         this.autor = autor;
+ 
+     }
+     //getter y setter
+     public String getIsbn(){
+         return this.isbn;
+     }
+     public void setIsbn(String isbn){
+         this.isbn = isbn;
+     }
+     public String getAutor(){
+         return this.autor;
+     }
+     public void setAutor(String autor){
+         this.autor = autor;
+     }
+ 
+     @Override
+     public String toString(){
+         
+         return " Autor: "+this.autor+" ISBN: "+this.isbn;
+ 
+     }
+
+}
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,11 +1,22 @@
 package miPrincipal;
 
+import java.util.Date;
+
 public class Principal {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new Principal().getGreeting());
+        Publicacion publicacion = new Publicacion();
+        publicacion.setEditorial("McGrawHill");
+        publicacion.setFecha(new Date("01/01/2000"));
+
+        Libro libro = new Libro();  
+        Libro libro1 = new Libro("21323313", "Sheakspeare");  
+        Libro libro2 = new Libro("Harry", new Date(12/01/2010));
+        
+        Revista revista = new Revista();
+        revista.setEditorial("SEP");
+        revista.setFecha(new DateFormat.parse("22/01/2022"));
+        revista.setPeriodicidad("anual");
+        revista.setEjemplares(2000);
     }
 }
