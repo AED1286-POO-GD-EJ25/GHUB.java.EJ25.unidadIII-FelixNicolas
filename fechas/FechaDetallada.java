1a9245bcdc41ea90ea2164d5d033214cee44d0c7
NicolasFelixAG <L24171081@culiacan.tecnm.mx>
2025-04-03 15:59:46 +0000

    ,ll

---
+++ fechas/FechaDetallada.java
@@ -0,0 +1,5 @@
+package fechas;
+public class FechaDetallada extends Fecha{
+    private static String meses[]={"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"}
+    Fecha fechaEscrita= new Fecha("22/")
+}
--- fechas/TestFecha4.java
+++ fechas/TestFecha4.java
@@ -2,6 +2,24 @@ package fechas;
 import java.util.Scanner;
 public class TestFecha4 {
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresa una Fecha (DD/MM/AAAA):");
+
+        //leemos la fecha
+        String sFecha = entrada.nextLine();
+
+        //creamos un objeto de la clase
+        Fecha f = new Fecha(sFecha);
+
+        //el usuario ingresa la cantidad de dias a sumar
+        System.out.print("Ingresa dias a sumar (puede ser negativo):");
+        int diasSum = entrada.nextInt();
+
+        //le sumanos esos dias a la fecha
+        f.addDias(diasSum);
+
+        //mostramos la nueva fecha
+        System.out.println("Sumando: "+f);
+
     }
 }
